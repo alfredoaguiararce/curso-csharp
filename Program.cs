fd6eb878dfff314d5ceec8f202cc9087d1e93491
alfredoaguiararce <67090401+alfredoaguiararce@users.noreply.github.com>
2022-07-21 12:51:26 -0600

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Por favor ingrese un nombre");
+var name = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su cargo");
+var job = Console.ReadLine();
+Console.WriteLine("Por favor ingrese un edad");
+var age = Console.ReadLine();
+
+Console.WriteLine($"Hola, mi nombre es {name}, trabajo como {job}, y tengo {age} de edad.");
